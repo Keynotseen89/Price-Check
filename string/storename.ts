f802c0391b8b7176941f9442149b6e1ecd1f3cb3
Keynotseen89 <ksintora@yahoo.com>
2023-11-24 13:02:34 -0800

    Create storename.ts
    enum of store names used for automation

---
+++ string/storename.ts
@@ -0,0 +1,11 @@
+export enum StoreName{
+    BestBuyStore = 'BestBuy',
+    AmazonStore = 'Amazon',
+    GlowforgeStore = 'Glowforge Store',
+    MichalesStore = 'Michales',
+    JoannStore = 'Joann',
+    NetgearStore = 'Netgear',
+    TargetStore = 'Target',
+    WalmartStore = 'Walmart',
+    StaplesStore = 'Staples',
+}
