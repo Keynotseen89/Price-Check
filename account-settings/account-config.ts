4c83d0ef2232772b2fbf286c8a5e8d530b7db695
Keynotseen89 <ksintora@yahoo.com>
2023-11-24 12:58:26 -0800

    Create account-config.ts
    Created an account list for email sending

---
+++ account-settings/account-config.ts
@@ -0,0 +1,13 @@
+/**
+ *  Update SERVICE with type of email server
+ *  Update PORT with different port if needed
+ *  Update USER with your email username 
+ *  Update PASS with email password or auth key
+ */
+export enum AccountSetting{
+    SERVICE = 'gmail',
+    HOST = '',
+    PORT = 587,
+    USER = '<user-name-for-email-account>',
+    PASS = '<password-or-key-for-login>',
+}
